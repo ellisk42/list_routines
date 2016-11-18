21855a884cd6291c4804dcbf2b82e5acbd98adff
Kevin Ellis <ellisk@mit.edu>
2016-11-18 17:01:29 -0500

    learn get

--- stochastic.py
+++ stochastic.py
@@ -1,6 +1,7 @@
 from math import log
 from collections import Counter
 import editdistance
+from random import random
 
 from LOTlib.Hypotheses.RecursiveLOTHypothesis import RecursiveLOTHypothesis
 from LOTlib.Eval import primitive
@@ -26,22 +27,35 @@ def myRest_(xs):
 # Define a grammar object
 grammar = Grammar(start='START')
 
-grammar.add_rule('START', '(%s if (myRest_(x) != []) else %s)', ['NUMBER','NUMBER'],1.0)
+grammar.add_rule('START', '(%s if %s else %s)', ['NUMBER','PREDICATE','START'],1.0)
+grammar.add_rule('START', '%s', ['NUMBER'],2.0)
 
 # Define some operations
 grammar.add_rule('LIST', 'myRest_', ['LIST'], 1.0)
 grammar.add_rule('LIST', 'cons_', ['NUMBER', 'LIST'], 1.0)
-# grammar.add_rule('LIST', '[]', None, 1.0)
-grammar.add_rule('LIST', 'x',None,1.0)
+grammar.add_rule('LIST', '[]', None, 2.)
+grammar.add_rule('LIST', 'x',None, 2.)
 
 grammar.add_rule('NUMBER', 'myFirst_', ['LIST'], 1.0)
-grammar.add_rule('NUMBER', 'recurse_', ['LIST'],1.0)
+grammar.add_rule('NUMBER', 'recurse_', ['NUMBER','LIST'],1.0)
+grammar.add_rule('NUMBER', '(%s + 1)', ['NUMBER'],0.5)
+grammar.add_rule('NUMBER', '(%s - 1)', ['NUMBER'],0.5)
+grammar.add_rule('NUMBER', '0', None,1.5)
+grammar.add_rule('NUMBER', '1', None,1.5)
+grammar.add_rule('NUMBER', 'i', None,1.5)
 
-#grammar.add_rule('PREDICATE', '(%s == [])', ['LIST'],1.0)
+grammar.add_rule('PREDICATE', '(%s == [])', ['LIST'],1.0)
+grammar.add_rule('PREDICATE', '(%s %s %s)', ['NUMBER', 'COMPARISON', 'NUMBER'],1.)
+#grammar.add_rule('PREDICATE', '(not (%s and %s))', ['PREDICATE','PREDICATE'],1.)
+
+grammar.add_rule('COMPARISON', ' < ', None, 1.)
+grammar.add_rule('COMPARISON', ' <= ', None, 1.)
+grammar.add_rule('COMPARISON', ' == ', None, 1.)
+
 
 class MyHypothesis(RecursiveLOTHypothesis):
     def __init__(self, **kwargs):
-        RecursiveLOTHypothesis.__init__(self, grammar=grammar, display="lambda recurse_, x: %s", **kwargs)
+        RecursiveLOTHypothesis.__init__(self, grammar=grammar, display="lambda recurse_, i, x: %s", **kwargs)
 
 #    def __call__(self,*args):
 #        try:
@@ -73,14 +87,20 @@ lastExamples = [(list_([8]), 8),
                 (list_([8,2,9,9,9]),9),
                 (list_([2]), 2)
                 ]
-data = [ FunctionData(input = [e[0]],output = e[1],alpha = 0.95)
-         for e in lastExamples ]
-
+getExamples = [[input[0], list_(input[1:]), input[input[0]]]
+               for input in [[1,9],
+                             [2,2,8],
+                             [2,3,3,5],
+                             [3,1,0,8,4],
+                             [3,4,0,7,1,47]] ]
+data = [ FunctionData(input = e[:-1], output = e[-1], alpha = 0.95)
+         for e in getExamples ]
+print data
 
 h0 = MyHypothesis()
 count = Counter()
-for h in MHSampler(h0,data,steps = 10000,likelihood_temperature=0.001):
+for h in MHSampler(h0,data,steps = 100000,prior_temperature=1):
     count[h] += 1
-for h in sorted(count.keys(),key = lambda x: count[x]):
+for h in sorted(count.keys(),key = lambda x: x.likelihood + x.prior):
     print count[h],h.likelihood,h.prior,h
 
