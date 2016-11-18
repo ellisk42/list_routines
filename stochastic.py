f3c40775757b85d48c5c739999f78be1880b3030
Kevin Ellis <ellisk@mit.edu>
2016-11-18 13:10:16 -0500

    stochastic search kind of working

--- stochastic.py
+++ stochastic.py
@@ -1,5 +1,6 @@
 from math import log
 from collections import Counter
+import editdistance
 
 from LOTlib.Hypotheses.RecursiveLOTHypothesis import RecursiveLOTHypothesis
 from LOTlib.Eval import primitive
@@ -8,66 +9,52 @@ from LOTlib.DataAndObjects import FunctionData
 from LOTlib.Grammar import Grammar
 from LOTlib.Hypotheses.LOTHypothesis import LOTHypothesis
 
+@primitive
+def myFirst_(xs):
+    if type(xs) is list and len(xs) == 2:
+        return xs[0]
+    else:
+        raise Exception('FIRST')
+
+@primitive
+def myRest_(xs):
+    if type(xs) is list and len(xs) == 2:
+        return xs[1]
+    else:
+        raise Exception('REST')
 
 # Define a grammar object
 grammar = Grammar(start='START')
 
-grammar.add_rule('START', 'if_', ['PREDICATE','NUMBER','START'],1.0)
-grammar.add_rule('START', '%s', ['RECURSIVENUMBER'],1.0)
+grammar.add_rule('START', '(%s if (myRest_(x) != []) else %s)', ['NUMBER','NUMBER'],1.0)
 
 # Define some operations
-if False:
-    grammar.add_rule('RECURSIVELIST', 'cdr_', ['RECURSIVELIST'], 1.0)
-    grammar.add_rule('RECURSIVELIST', 'cons_', ['NUMBER', 'RECURSIVELIST'], 1.0)
-    grammar.add_rule('RECURSIVELIST', '[]', None, 1.0)
-    grammar.add_rule('RECURSIVELIST', 'x',None,1.0)
-    grammar.add_rule('RECURSIVELIST', 'recurse_', ['RECURSIVELIST'],1.0)
-
-
-grammar.add_rule('LIST', 'cdr_', ['LIST'], 1.0)
-grammar.add_rule('LIST', 'cons_', ['NUMBER', 'LIST'], 1.0)
-grammar.add_rule('LIST', '[]', None, 1.0)
+grammar.add_rule('LIST', 'myRest_', ['LIST'], 1.0)
+# grammar.add_rule('LIST', 'cons_', ['NUMBER', 'LIST'], 1.0)
+# grammar.add_rule('LIST', '[]', None, 1.0)
 grammar.add_rule('LIST', 'x',None,1.0)
-#grammar.add_rule('LIST', 'recurse_', ['LIST'],1.0)
 
+grammar.add_rule('NUMBER', 'myFirst_', ['LIST'], 1.0)
+grammar.add_rule('NUMBER', 'recurse_', ['LIST'],1.0)
 
-#grammar.add_rule('FUNCTION', 'lambda',['LIST'],1.0,bv_type = 'LIST')
-
-grammar.add_rule('NUMBER', 'car_', ['LIST'], 1.0)
-#grammar.add_rule('NUMBER', '0', None, 1.)
-
-grammar.add_rule('RECURSIVENUMBER', 'car_', ['LIST'], 1.0)
-#grammar.add_rule('RECURSIVENUMBER', 'plus_(%s,1)', ['RECURSIVENUMBER'], 1.)
-#grammar.add_rule('RECURSIVENUMBER', 'plus_(%s,-1)', ['RECURSIVENUMBER'], 1.)
-#grammar.add_rule('RECURSIVENUMBER', '0', None, 1.)
-grammar.add_rule('RECURSIVENUMBER', 'recurse_', ['LIST'],1.0)
-
-# And define some numbers. We'll give them a 1/n^2 probability
-if False:
-    for n in xrange(1,10):
-        grammar.add_rule('NUMBER', str(n), None, 10.0/n**2)
-        grammar.add_rule('RECURSIVENUMBER', str(n), None, 10.0/n**2)
-
-
-
-grammar.add_rule('PREDICATE', '(%s == [])', ['LIST'],1.0)
-# grammar.add_rule('PREDICATE', 'or_', ['PREDICATE','PREDICATE'],1.0)
-# grammar.add_rule('PREDICATE', 'not_', ['PREDICATE'],1.0)
-# grammar.add_rule('PREDICATE', '(%s < %s)', ['NUMBER','NUMBER'],1.0)
-# grammar.add_rule('PREDICATE', '(%s == %s)', ['NUMBER','NUMBER'],1.0)
+#grammar.add_rule('PREDICATE', '(%s == [])', ['LIST'],1.0)
 
 class MyHypothesis(RecursiveLOTHypothesis):
     def __init__(self, **kwargs):
         RecursiveLOTHypothesis.__init__(self, grammar=grammar, display="lambda recurse_, x: %s", **kwargs)
 
-    def __call__(self,*args):
-        try:
-            return RecursiveLOTHypothesis.__call__(self,*args)
-        except TypeError:
-            return None
+#    def __call__(self,*args):
+#        try:
+#            return RecursiveLOTHypothesis.__call__(self,*args)
+#        except TypeError:
+#            return None
 
     def compute_single_likelihood(self, datum):
         try:
+            # print 'program: ', str(self)
+            # print 'input: ', datum.input
+            # print 'our output: ', datum.output
+            # print 'their output: ', self(*datum.input)
             if str(self(*datum.input)) == str(datum.output):
                 return log(datum.alpha)
             else:
@@ -86,17 +73,14 @@ lastExamples = [(list_([8]), 8),
                 (list_([8,2,9,9,9]),9),
                 (list_([2]), 2)
                 ]
-print lastExamples
 data = [ FunctionData(input = [e[0]],output = e[1],alpha = 0.95)
          for e in lastExamples ]
-print data
 
 
 h0 = MyHypothesis()
 count = Counter()
-for h in MHSampler(h0,data,steps = 1000000):
+for h in MHSampler(h0,data,steps = 10000,likelihood_temperature=0.001):
     count[h] += 1
-#    print h.compute_prior(),h.compute_likelihood(data),h
 for h in sorted(count.keys(),key = lambda x: count[x]):
-    print count[h],h.likelihood,h
+    print count[h],h.likelihood,h.prior,h
 
